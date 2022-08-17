2adc0935d48e8c46eddb7e5119a50a1b9598e8e1
DmitryKlochenok <106827836+DmitryKlochenok@users.noreply.github.com>
2022-08-17 12:10:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from pip._internal.resolution.resolvelib import candidates
+
 import utils
 
 utils.load_candidates_from_json()
@@ -6,11 +8,12 @@ utils.load_candidates_from_json()
 app = Flask(__name__)
 
 @app.route("/")
-def page_index(candidates):
-   return render_template("list.html", candidates=candidates)
+def page_index():
+    candidates=utils.load_candidates_from_json()
+    return render_template("list.html", candidates=candidates)
 
 
-@app.route("/candidate/<int: x>")
+@app.route("/candidate/<int:x>")
 def page_candidate(x):
   cand_by_id = utils.get_candidate(x)
   page = render_template("card.html", cand_by_id=cand_by_id)
@@ -18,14 +21,17 @@ def page_candidate(x):
 
 @app.route("/search/<candidate_name>")
 def page_name(candidate_name):
+    candidates = utils.load_candidates_from_json()
     cands_by_name = utils.get_candidates_by_name(candidate_name)
     page = render_template("search.html", cands_by_name=cands_by_name)
     return page
 
 @app.route("/search/<skill_name>")
 def page_skills(skill_name):
+    candidates = utils.load_candidates_from_json()
     cands_by_skills = utils.get_candidates_by_skill(skill_name)
     page = render_template("skill.html", cands_by_skills=cands_by_skills)
     return page
 
-app.run()
+if __name__ == "__main__":
+    app.run()
